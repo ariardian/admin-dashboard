94429225b221601d23fb694679d59f33dc37c545
ariardian <ary.ardyan@gmail.com>
2020-03-17 09:20:48 +0700

    edit warn

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './App.scss';
 import NavMain from './containers/admin/navmain';
 import LandingPage from './containers/page/landingpage';
-import Login from './containers/page/login'
-import { Container, Row, Col } from "reactstrap";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+// import Login from './containers/page/login'
+import { Container } from "reactstrap";
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
   return (
@@ -21,15 +21,15 @@ function App() {
     </React.Fragment>
   );
 }
-function Home() {
-  return <h2>Home</h2>;
-}
+// function Home() {
+//   return <h2>Home</h2>;
+// }
 
-function About() {
-  return <Login />;
-}
+// function About() {
+//   return <Login />;
+// }
 
-function Users() {
-  return <h2>Users</h2>;
-}
+// function Users() {
+//   return <h2>Users</h2>;
+// }
 export default App;
